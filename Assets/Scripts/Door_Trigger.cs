c59aabdfba5084b535bbb276ec9bd5d16bb8ffe3
MILKS3NPAI <reggaming01@gmail.com>
2020-10-06 13:02:28 -0700

    -Created Animated Door -Made NPC Prefab

---
+++ Assets/Scripts/Door_Trigger.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door_Trigger : MonoBehaviour
+{
+    [SerializeField] private Door_Trigger door; 
+    private Animator animator;
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+    private void Update()
+    {
+      if (Input.GetKeyDown(KeyCode.E))
+      {
+        animator.SetBool("Open", true);
+      }
+
+
+      if (Input.GetKeyDown(KeyCode.Q))
+      {
+         animator.SetBool("Open", false);
+      }
+
+  
+    }
+    
+}
