8b798724f77a642498d16cbfbae86cfe075d88f6
MILKS3NPAI <reggaming01@gmail.com>
2020-10-08 18:34:44 -0700

    -Player cannot move while in dialogue anymore

--- Assets/Scripts/Player_Controls.cs
+++ Assets/Scripts/Player_Controls.cs
@@ -1,31 +1,41 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Fungus;
 
 public class Player_Controls : MonoBehaviour
 {
     [SerializeField] private LayerMask platformsLayerMask;
     private Rigidbody2D body;
     private BoxCollider2D boxCollider2d;
+    public Fungus.Flowchart myFlowchart;
     private float movespeed = 7f;
     private float jumpspeed = 65f;
     private float movement;
-   
+    public bool player_in_dialogue;
   
     void Start()
     {
+        myFlowchart = FindObjectOfType<Flowchart>();
         body = GetComponent<Rigidbody2D>();
         boxCollider2d = transform.GetComponent<BoxCollider2D>();
     }
     private void Update()
     {
-        Jump();
+        player_in_dialogue = myFlowchart.GetBooleanVariable("player_in_chat");
+        if (player_in_dialogue == false)
+        {
+            Jump();
+        }
     }
 
     void FixedUpdate()
-    {  
-        movement = Input.GetAxis("Horizontal") * movespeed;
-        body.velocity = new Vector3(movement, body.velocity.y, 0f);
+    {
+        if (player_in_dialogue == false)
+        {
+            movement = Input.GetAxis("Horizontal") * movespeed;
+            body.velocity = new Vector3(movement, body.velocity.y, 0f);
+        }
     }
    
 
@@ -42,6 +52,7 @@ public class Player_Controls : MonoBehaviour
         RaycastHit2D raycastHit2d = Physics2D.BoxCast(boxCollider2d.bounds.center, boxCollider2d.bounds.size, 0f, Vector2.down, .1f, platformsLayerMask);
         return raycastHit2d.collider != null;
     }
+
     //private bool WallCollision()
     //{
     //    RaycastHit2D raycastHit2dleft = Physics2D.BoxCast(boxCollider2d.bounds.center, boxCollider2d.bounds.size, 0f, Vector2.left, .1f, platformsLayerMask);
