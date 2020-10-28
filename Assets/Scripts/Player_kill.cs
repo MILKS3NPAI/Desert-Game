2b951ffc8c402c6932d065d90ece3c07f37be599
Ron Riley Co <60789953+MILKS3NPAI@users.noreply.github.com>
2020-10-28 12:00:10 -0700

    Created player kill function and respawn

---
+++ Assets/Scripts/Player_kill.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_kill : MonoBehaviour
+{
+    [SerializeField] Transform spawnPoint;
+    // Start is called before the first frame update
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.transform.CompareTag("Player"))
+            collision.transform.position = spawnPoint.position;
+    }
+}
