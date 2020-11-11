ebe1ec799b7bf5fd8d6808b503d7486dc11210b7
Ron Riley Co <60789953+MILKS3NPAI@users.noreply.github.com>
2020-11-11 11:16:20 -0800

    -Organized some files -Started on pit level OPEN IN THE SCENES FOLDER -2 new moving platforms -updated prefabs

---
+++ Assets/Scripts/Moving_Platforms/Moving_PlatformV.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Moving_PlatformV : MonoBehaviour
+{
+    private Rigidbody2D body;
+    public float dirY, movespeed = 4f;
+    public float max;
+    public float min;
+    bool moveUp = true;
+    public bool move = false;
+    float movement;
+
+
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+
+    void Update()
+    {
+        if (move == false)
+        {
+            body.velocity = new Vector3(0f, 0f, 0f);
+        }
+        if (move == false)
+        {
+            move = true;
+        }
+        else
+        {
+            move = false;
+        }
+
+
+
+
+        if (transform.position.y > max && move == true)
+        {
+            moveUp = false;
+        }
+        if (transform.position.y < min && move == true)
+        {
+            moveUp = true;
+        }
+        if (move == true)
+        {
+
+
+            if (moveUp)
+            {
+                transform.position = new Vector2(transform.position.x , transform.position.y + movespeed * Time.deltaTime);
+
+            }
+            else
+            {
+                transform.position = new Vector2(transform.position.x, transform.position.y - movespeed * Time.deltaTime);
+
+            }
+        }
+    }
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            collision.transform.parent = transform;
+        }
+    }
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            collision.transform.parent = null;
+        }
+    }
+}
---
+++ Assets/Scripts/Moving_Platforms/Moving_Platform_NOswitch.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Moving_Platform_NOswitch : MonoBehaviour
+{
+    private Rigidbody2D body;
+    float dirX, movespeed = 4f;
+    bool moveRight = true;
+    public bool move = false;
+    float movement;
+
+
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+
+    void Update()
+    {
+        if (move == false)
+        {
+            body.velocity = new Vector3(0f, 0f, 0f);
+        }   
+               if (move == false)
+                {
+                    move = true;
+                }
+                else
+                {
+                    move = false;
+                }
+            
+        
+
+
+        if (transform.position.x > 4f && move == true)
+        {
+            moveRight = false;
+        }
+        if (transform.position.x < -4.5f && move == true)
+        {
+            moveRight = true;
+        }
+        if (move == true)
+        {
+
+
+            if (moveRight)
+            {
+                transform.position = new Vector2(transform.position.x + movespeed * Time.deltaTime, transform.position.y);
+                
+            }
+            else
+            {
+                transform.position = new Vector2(transform.position.x - movespeed * Time.deltaTime, transform.position.y);
+             
+            }
+        }
+    }
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            collision.transform.parent = transform;
+        }
+    }
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            collision.transform.parent = null;
+        }
+    }
+}
